37a3447497f1e83ac6abfed2aebbe2bd66c47b15
Majo247 <cruzsanchezmaria600@gmail.com>
2024-10-17 15:08:39 -0600

    Crear una vista de inicio 17/10/24

---
+++ practicaRepaso/resources/views/inicio.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/js/app.js'])
+</head>
+<body>
+    <div>
+    <h1>Universidad Politécnica de Querétaro</h1>
+    <p>
+    Repaso 1<br>    
+    Cruz Sánchez María José<br>
+    Ingeniería en Sistemas Computacionales<br>
+    Iván Isay Guerra López<br>
+    S193<br>
+    17/10/24<br>
+    </p>
+    <a href="" class="btn btn-primary">REPASO 1</a>
+</div>
+</body>
+</html>
