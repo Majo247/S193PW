4f378c72b955fe4b12325dd5e8b9e4f8bddb6653
Majo247 <cruzsanchezmaria600@gmail.com>
2024-10-31 02:59:39 -0600

    3RepasoII31/10/24

--- practicaRepasoII/app/Http/Controllers/ControladorVista.php
+++ practicaRepasoII/app/Http/Controllers/ControladorVista.php
@@ -19,6 +19,19 @@ class ControladorVista extends Controller
 
     public function procesarLibro(Request $peticion)
     {
+        $validacion = $peticion->validate([
+            'txtisbn' => 'required|numeric|digits:13', 
+            'txttitulo' => 'required|string|max:150', 
+            'txtautor' => 'required|string|max:100', 
+            'txtpaginas' => 'required|integer|min:1', 
+            'txtaño' => 'required|integer|digits:4|min:1000|max:' . date('Y'), 
+            'txteditorial' => 'required|string|max:100',
+            'txtemail' => 'required|email'
+        ]);
+
+
+
+
         $titulo = $peticion->input('txttitulo');
         session()->flash('exito', 'Se guardó el Libro: ' . $titulo);
         return to_route('rutaRegistro');
--- practicaRepasoII/resources/views/Registro.blade.php
+++ practicaRepasoII/resources/views/Registro.blade.php
@@ -23,24 +23,29 @@
             <div class="mb-3">
                 <label for="isbn" class="form-label">ISBN:</label>
                 <input type="text" class="form-control" name="txtisbn" value="{{old('txtisbn')}}">
+                <small class="text-danger fst-italic">{{$errors->first('txtisbn')}}</small>
             </div>
             <div class="mb-3">
                 <label for="isbn" class="form-label">Titulo:</label>
                 <input type="text" class="form-control" name="txtisbn" value="{{old('txttitulo')}}">
+                <small class="text-danger fst-italic">{{$errors->first('txttitulo')}}</small>
             </div>
             <div class="mb-3">
                 <label for="isbn" class="form-label">Autor:</label>
                 <input type="text" class="form-control" name="txtautor" value="{{old('txtautor')}}">
+                <small class="text-danger fst-italic">{{$errors->first('txtautor')}}</small>
             </div>
 
             <div class="mb-3">
                 <label for="isbn" class="form-label">Paginas:</label>
                 <input type="text" class="form-control" name="txtpaginas" value="{{old('txtpaginas')}}">
+                <small class="text-danger fst-italic">{{$errors->first('txtpaginas')}}</small>
             </div>
 
             <div class="mb-3">
                 <label for="isbn" class="form-label">Año:</label>
                 <input type="text" class="form-control" name="txtaño" value="{{old('txtaño')}}">
+                <small class="text-danger fst-italic">{{$errors->first('txtaño')}}</small>
             </div>
 
             <div class="mb-3">
@@ -59,6 +64,15 @@
     </div>
 
  </div>
+ <footer>
+    <div class="container-fluid">
+        <div class="row text-center">
+         <p>
+            © 2024 PREMIUM. Todos los derechos reservados.
+         </p>
+         </div>
+         </div>
+</footer>
 
 
 @endsection
