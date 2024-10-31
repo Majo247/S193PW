b4b81622672a48aa6c23ba13ed51ee50e2991fb1
Majo247 <cruzsanchezmaria600@gmail.com>
2024-10-31 04:05:24 -0600

    5RepasoII31/10/24

--- practicaRepasoII/resources/views/Registro.blade.php
+++ practicaRepasoII/resources/views/Registro.blade.php
@@ -2,9 +2,7 @@
 @section('contenido')
 
 <div class="card font-monospace">
-    <div class="card-header fs-5 text-center text-primary">
-        Registro de Libros
-    </div>
+    
 
     @if(session('exito'))
         <script> 
@@ -16,54 +14,60 @@
         </script>
     @endif
 
-    <div class="card-body text-justify">
-        <form action="{{ route('rutaLibro') }}" method="POST">
-            @csrf
+    <div class="card font-monospace">
+       <div class="card-header fs-5 text-center text-primary">{{__('Book Registration')}}</div>
+       <div class="card-body text-justify">
+            <form action="{{ route('rutaLibro') }}" method="POST">
+         
+              @csrf
 
-            <div class="mb-3">
-                <label for="isbn" class="form-label">ISBN:</label>
+             <div class="mb-3">
+                <label for="isbn" class="form-label"> {{__('ISBN')}}</label>
                 <input type="text" class="form-control" name="txtisbn" value="{{old('txtisbn')}}">
                 <small class="text-danger fst-italic">{{$errors->first('txtisbn')}}</small>
-            </div>
-            <div class="mb-3">
-                <label for="isbn" class="form-label">Titulo:</label>
+             </div>
+             <div class="mb-3">
+                <label for="isbn" class="form-label"> {{__('Titulo')}}</label>
                 <input type="text" class="form-control" name="txtisbn" value="{{old('txttitulo')}}">
                 <small class="text-danger fst-italic">{{$errors->first('txttitulo')}}</small>
-            </div>
-            <div class="mb-3">
-                <label for="isbn" class="form-label">Autor:</label>
+             </div>
+             <div class="mb-3">
+                <label for="isbn" class="form-label"> {{__('Autor')}}</label>
                 <input type="text" class="form-control" name="txtautor" value="{{old('txtautor')}}">
                 <small class="text-danger fst-italic">{{$errors->first('txtautor')}}</small>
-            </div>
+             </div>
 
-            <div class="mb-3">
-                <label for="isbn" class="form-label">Paginas:</label>
+             <div class="mb-3">
+                <label for="isbn" class="form-label"> {{__('Paginas')}}</label>
                 <input type="text" class="form-control" name="txtpaginas" value="{{old('txtpaginas')}}">
                 <small class="text-danger fst-italic">{{$errors->first('txtpaginas')}}</small>
-            </div>
+             </div>
 
-            <div class="mb-3">
-                <label for="isbn" class="form-label">Año:</label>
+             <div class="mb-3">
+                <label for="isbn" class="form-label"> {{__('Año')}}</label>
                 <input type="text" class="form-control" name="txtaño" value="{{old('txtaño')}}">
                 <small class="text-danger fst-italic">{{$errors->first('txtaño')}}</small>
-            </div>
+             </div>
 
-            <div class="mb-3">
-                <label for="isbn" class="form-label">Editorial:</label>
+             <div class="mb-3">
+                <label for="isbn" class="form-label"> {{__('Editorial')}}</label>
                 <input type="text" class="form-control" name="txteditorial" value="{{old('txteditorial')}}">
-            </div>
+             </div>
 
-            <div class="mb-3">
-                <label for="isbn" class="form-label">Email de Editorial:</label>
+             <div class="mb-3">
+                <label for="isbn" class="form-label">{{__('Email de Editorial')}}</label>
                 <input type="text" class="form-control" name="txtemail" value="{{old('txtemail')}}">
-            </div>
+             </div>
 
-            <button type="submit" class="btn btn-success btn-sm">Guardar Libro</button>
-        </form>
+             <button type="submit" class="btn btn-success btn-sm">{{__('Guardar libro')}}</button>
+        
+            </form>
+        </div>  
 
     </div>
 
- </div>
+</div>
+
  <footer>
     <div class="container-fluid">
         <div class="row text-center">
--- practicaRepasoII/resources/views/layouts/plantilla.blade.php
+++ practicaRepasoII/resources/views/layouts/plantilla.blade.php
@@ -12,17 +12,17 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">2° Practica</a>
+          <a class="navbar-brand" href="#">{{__('2° Practice ')}}</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link {{ request()->routeIs('rutaPrincipal') ? 'text-warning' : '' }}" href="{{ route('rutaPrincipal') }}">Principal</a>
+                <a class="nav-link {{ request()->routeIs('rutaPrincipal') ? 'text-warning' : '' }}" href="{{ route('rutaPrincipal') }}">{{__('Main')}}</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link {{ request()->routeIs('rutaRegistro') ? 'text-warning' : '' }}" href="{{ route('rutaRegistro') }}">Registro de libros</a>
+                <a class="nav-link {{ request()->routeIs('rutaRegistro') ? 'text-warning' : '' }}" href="{{ route('rutaRegistro') }}">{{__('Registro de libros')}}</a>
               </li>
             </ul>
           </div>
