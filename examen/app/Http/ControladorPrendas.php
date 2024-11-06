bedf056eee245da20ba9036bc6776669bed80584
Majo247 <cruzsanchezmaria600@gmail.com>
2024-11-05 20:39:36 -0600

    final

--- examen/app/Http/ControladorPrendas.php
+++ examen/app/Http/ControladorPrendas.php
@@ -1,4 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
-namespace App\Http\ControladorPrendas;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\ValidarRegistro;
+
+class ControladorRegistro extends Controller
+{
+    
+public function abrirFormulario(){
+
+ return view('formRegistro');
+}
+
+public function GuardarRgistro(ValidarRegistro $fritura){
+
+    $guardar = $fritura->input('Nombre');
+    session()->flash("EXITO","Se ha registrado correctamente " .$guardar);
+    return view('formRegistro');
+   }
+
+   
+   
+}
--- examen/resources/views/Registro.blade.php
+++ examen/resources/views/Registro.blade.php
@@ -1,6 +1,16 @@
 @extends('layout.navformularios')
 @section('contenido')
 
+@session('EXITO')
+<div 
+    class="alert alert-primary"
+    role="alert"
+    >
+    <strong>Registro exitoso</strong> {{$value}}
+</div>
+@endsession
+
+
 <h1 class="text-center mb-4">Registro fde clientes</h1> 
 
 
@@ -9,22 +19,25 @@
         <div class="mb-3">
           <label for="username" class="form-label">Prenda</label>
           <input type="text" class="form-control" id="prenda" placeholder="Ingresa la prenda" required>
+          <small>{{ $errors->first('txtPrenda') }}</small>
         </div>
 
         <div class="mb-3">
           <label for="username" class="form-label">Color</label>
-          <input type="text" class="form-control" id="prenda" placeholder="Ingresa la prenda" required>
+          <input type="text" class="form-control" id="color" placeholder="Ingresa el color" required>
+          <small>{{ $errors->first('txtColor') }}</small>
         </div>
 
         <div class="col-md-4">
         <label for="ninos" class="form-label">cantida</label>
         <select class="form-select" id="ninos">
-          <option selected>Selecciona</option>
+          <option selected>Selecciono</option>
           <option value="0">0</option>
           <option value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
           <option value="4">4</option>
+          <small>{{ $errors->first('txtcantidsad') }}</small>
         </select>
 
         <button type="submit" class="btn btn-primary w-100">Guardar Prendas</button>
